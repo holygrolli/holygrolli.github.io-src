26f3a491500828e5defdb892a83c34f70c2d035b
Andreas Groll <andreas.groll@gmail.com>
2018-02-11 15:55:08 +0100

    changes to reflect responsive bg images in theme optimized build.sh to execute a dry-run

--- build.sh
+++ build.sh
@@ -1,16 +1,48 @@
-#!/bin/sh
-#set -o errexit
+#!/bin/bash
+set -o errexit
 
-#s3cmd sync -n --access_key=${AWS_ACCESS_KEY} --
-aws s3 sync static/img/ s3://img.networkchallenge.de --size-only
-#--dryrun
+show_help() {
+    echo \"--dry-run\" for only upload images
+    echo \"publish\" for deployment
+}
 
-git clone https://github.com/adulescentulus/adulescentulus.github.io.git /tmp/target
-git submodule init
-git submodule update
+OPT_DRYRUN=0
+OPT_PUBLISH=0
 
-hugo -d /tmp/target
-cd /tmp/target
-git add .
-git commit -am "content update"
-git push origin master
+while [ $# -gt 0 ]
+do
+    case "$1" in
+        --dry-run)
+            OPT_DRYRUN=1
+            ;;
+        publish)
+            OPT_PUBLISH=1
+            ;;
+        -h|help)
+            show_help
+            exit 0
+            ;;
+    esac
+    shift
+done
+
+if [[ $OPT_DRYRUN -eq 1 ]]; then
+    aws s3 sync static/img/ s3://img.networkchallenge.de --size-only --dryrun
+else
+    aws s3 sync static/img/ s3://img.networkchallenge.de --size-only
+fi
+
+if [[ $OPT_PUBLISH -eq 1 ]]; then
+    echo publishing ...
+    git clone https://github.com/adulescentulus/adulescentulus.github.io.git /tmp/target
+    git submodule init
+    git submodule update
+
+    hugo -d /tmp/target
+    cd /tmp/target
+    git add .
+    git commit -am "content update"
+    git push origin master
+else
+    echo missing publish argument, doing nothing
+fi
